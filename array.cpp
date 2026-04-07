7fbbef41dd2a440f8bd28b59bbc3914469cdd2ab
aswarina <lyxsyyawa@gmail.com>
2026-04-07 18:14:05 +0700

    tambah output untuk menampilkan data manual

--- array.cpp
+++ array.cpp
@@ -22,6 +22,13 @@ int main()
     cin >> datamu [4];
     cout << endl;
 
+    cout << "data pertama = " << datamu[0] << endl;
+    cout << "data kedua = " << datamu[1] << endl;
+    cout << "data ketiga = " << datamu[2] << endl;
+    cout << "data keempat = " << datamu[3] << endl;
+    cout << "data kelima = " << datamu[4] << endl;
+    cout << endl;
+
 
 
   
